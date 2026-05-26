69f57b66bec3cd866484378e248d05405a9a38ca
Praveen Roy <roykumar0593@gmail.com>
2026-05-26 22:57:15 +0530

    added test structure

--- tests/example3.spec.ts
+++ tests/example3.spec.ts
@@ -1,21 +1,53 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, request} from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// const {test, expect} = require('@playwright/test') - JS
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+// fixture - Special object which help us in setting up and tear down the process
+// page, browser, context, request
+
+// page - browser fixture
+
+
+test('get started link',   async ({ page }) => {
+
+   await page.goto('https://playwright.dev/');
+   await page.getByRole('link', { name: 'Get started' }).click();
+   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+// Promise - Pending, success , failure
 
 
 // Playwright by default runs in headless mode - Browser will no open
+
+
+//import {test, expect} from '@playwright/test'
+
+// test() - Helps us in terms of writing the test cases in Playwright
+// expect() - It helps us in terms of writing the assertion in playwright
+
+
+test("Title", function(){
+    //......
+
+    // Launch the url
+    // fill the username
+    // fill the password
+    // click on login button
+    // validate if the login is successful or not
+
+})
+
+test("Title1", ()=>{
+
+})
+
+// async- await
+
+
+// async - asynchronous
+
+
+// JS & TS are asynchronous in nature
+
